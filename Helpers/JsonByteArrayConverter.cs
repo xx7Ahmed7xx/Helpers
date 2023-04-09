91c51612c506b507a9e894a45d9d3b52e5d196c2
Ahmed Ayman Mansour <ahmadahmad00790@yahoo.com>
2023-04-09 15:58:23 +0200

    Updated Targeted frameworks to support .NET 5,6,7 Added JwtClassConverter for string to byteArray and vice versa.

---
+++ Helpers/JsonByteArrayConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    /// <summary>
+    /// Supports converting from Base64 String into JsonByteArray, and vice versa.
+    /// </summary>
+    public class JsonByteArrayConverter : JsonConverter<byte[]?>
+    {
+        /// <summary>
+        /// Converts base64 encoded string to byte[].
+        /// </summary>
+        /// <param name="reader">The Utf8JsonReader object.</param>
+        /// <param name="typeToConvert">The target type.</param>
+        /// <param name="options">JsonSerializer options object.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public override byte[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TryGetBytesFromBase64(out byte[]? result))
+            {
+                throw new Exception("The value (" + reader.GetString() + ") couldn't be encoded successfully.");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Converts byte[] to base64 encoded string.
+        /// </summary>
+        /// <param name="writer">The Utf8JsonReader object.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="options">JsonSerializer options object.</param>
+        public override void Write(Utf8JsonWriter writer, byte[]? value, JsonSerializerOptions options)
+        {
+            writer.WriteBase64StringValue(value);
+        }
+    }
+}
